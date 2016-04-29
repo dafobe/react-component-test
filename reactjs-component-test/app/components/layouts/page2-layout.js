06107756c5b1e6f515660092af573f541da808b0
David Fonollosa <david.fonollosa@innofis.com>
2016-04-29 14:45:25 +0200

    added layouts

---
+++ reactjs-component-test/app/components/layouts/page2-layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Page2Layout = React.createClass({
+  render: function() {
+    return (
+      <h2>Page 2</h2>
+      <section>
+        Page 2 content
+      </section>
+    );
+  }
+});
+
+export default Page2Layout;
